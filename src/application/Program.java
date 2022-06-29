7bf8804ba5a5b178a871dd3003aaafb2a6c117ab
Leandro <souzaleandro79@gmail.com>
2022-06-28 21:13:46 -0300

    Alteração para chamada em métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,42 +8,40 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner (System.in);
 
 		Triangle x,y;
 		x = new Triangle();
 		y = new Triangle();
-		
+
 		System.out.println("Enter the measures of triangle X: ");
-		
+
 		x.a = scan.nextDouble();
 		x.b = scan.nextDouble();
 		x.c = scan.nextDouble();
-		
+
 		System.out.println("Enter the measures of triangle Y: ");
-		
+
 		y.a = scan.nextDouble();
 		y.b = scan.nextDouble();
 		y.c = scan.nextDouble();
-		
-		double p  = (x.a + x.b + x.c) /2;
-		double areaX = Math.sqrt (p*(p - x.a)*(p - x.b)* (p -x.c));
-		
-		p = (y.a + y.b + y.c) /2;
-		double areaY = Math.sqrt (p*(p - y.a)*(p - y.b)* (p - y.c));
-		
+
+		//Chamada do Metodo
+		double areaX = x.area();
+		double areaY = y.area();
+
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		
+
 		if( areaX > areaY) {
 			System.out.println("Larger area: X ");
 		}
 		else {
 			System.out.println("Larger area: Y ");
-		}	
-		
+		}
+
 		scan.close();
 
 	}
